7fdd51acc4f7b6fb295e9331991fdcf88e0b35cb
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-10-05 14:19:52 +0800

    feat: 安裝 react-router-dom。新增三頁 page。

---
+++ src/routes.tsx
@@ -0,0 +1,42 @@
+import type { RouteObject } from 'react-router-dom';
+import Home from './pages/Home';
+import Following from './pages/Following';
+import NotFound from './pages/NotFound';
+
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <Home />,
+    children: [],
+  },
+  {
+    path: "/following",
+    element: <Following />,
+    children: [],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+    children: [],
+  },
+];
+
+export default routes;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
