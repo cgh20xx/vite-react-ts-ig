f3ee06539ca2a032763933c713ecb274ceefebfd
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-10-06 16:54:38 +0800

    feat: 新增 Tailwind CSS IntelliSense VS Code 套件

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 const Home: React.FC = () => {
-  return <h1 className="text-3xl font-bold underline">Home</h1>;
+  return <h1 className="text-3xl font-bold text-red-500">Home</h1>;
 };
 
 export default Home;
