c773fe421512ab87bc6b1c3731c6b35464f7e696
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-10-18 16:19:06 +0800

    feat: 新增 IGPost 及 Comment 元件，測試單一貼文。

---
+++ src/components/IGPost/index.tsx
@@ -0,0 +1,40 @@
+import IGUser from 'components/IGUser';
+import Comment from './Comment';
+
+type IGPostProps = {
+  account: string;
+  location: string;
+  avatar: string;
+  photo: string;
+  likes: number;
+  description: string;
+  hashTags: string;
+  createTime: string;
+};
+
+const IGPost: React.FC<IGPostProps> = ({
+  account,
+  location,
+  avatar,
+  photo,
+  likes,
+  description,
+  hashTags,
+  createTime
+}) => {
+  return (
+    <div className="pb-4 shadow-md lg:mb-8">
+      <IGUser account={account} location={location} avatar={avatar} />
+      <img src={photo} />
+      <Comment
+        likes={likes}
+        account={account}
+        description={description}
+        hashTags={hashTags}
+        createTime={createTime}
+      />
+    </div>
+  );
+};
+
+export default IGPost;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import IGContainer from 'components/IGContainer';
 import IGHeader from 'components/IGHeader';
+import IGPost from 'components/IGPost';
 import IGStory from 'components/IGStory';
 import IGUser from 'components/IGUser';
 
@@ -11,6 +12,16 @@ const Home: React.FC = () => {
         <div className="flex justify-between">
           <div className="w-full lg:w-[600px]">
             <IGStory />
+            <IGPost
+              account={'abc'}
+              location={'Taipei'}
+              avatar={'/images/avatars/a1.png'}
+              photo={'/images/posts/main1.png'}
+              likes={99}
+              description={'說明文字'}
+              hashTags={'#測試'}
+              createTime={'1 天前'}
+            />
           </div>
           <div className="hidden w-[424px] lg:block">
             IGProfile
