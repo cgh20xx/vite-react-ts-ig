560e11defa049165bb80c0643cbfabad4a2a6cb9
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-10-20 16:36:55 +0800

    feat: 新增 Loading 元件，首頁動態牆新增戴入 loading 效果。

--- src/components/IGStory/index.tsx
+++ src/components/IGStory/index.tsx
@@ -1,17 +1,26 @@
+import Loading from 'components/Loading';
 import Item from './Item';
 // import db from '../../../mock/db.json';
 
 import { useGetIGStoriesQuery } from 'services/homeServices';
 
 const IGStory: React.FC = () => {
-  const { data } = useGetIGStoriesQuery('all');
+  const { data, isLoading } = useGetIGStoriesQuery('all');
   return (
-    <div className="no-scrollbar flex h-[110px] w-full items-center overflow-x-auto overflow-y-hidden shadow-md lg:my-8">
-      {data?.map(item => {
-        const { id, name, avatar } = item;
-        return <Item key={id} name={name} avatar={avatar} />;
-      })}
-    </div>
+    <>
+      {isLoading ? (
+        <div className="flex h-[110px] w-full items-center justify-center shadow-md lg:my-8">
+          <Loading />
+        </div>
+      ) : (
+        <div className="no-scrollbar flex h-[110px] w-full items-center overflow-x-auto overflow-y-hidden shadow-md lg:my-8">
+          {data?.map(item => {
+            const { id, name, avatar } = item;
+            return <Item key={id} name={name} avatar={avatar} />;
+          })}
+        </div>
+      )}
+    </>
   );
 };
 
