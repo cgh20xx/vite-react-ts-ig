2533f28a000c545eb3e78f5d1d8614ebedc4eaf4
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-10-21 11:08:48 +0800

    feat: 修改動態牆 data 接 json server api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,38 +3,45 @@ import IGHeader from 'components/IGHeader';
 import IGPost from 'components/IGPost';
 import IGStory from 'components/IGStory';
 import IGProfile from 'components/IGProfile';
-import db from '../../mock/db.json';
+import { useGetIGPostsQuery } from 'services/homeServices';
+import Loading from 'components/Loading';
 
 const IGPostList: React.FC = () => {
-  const data = db.posts;
+  const { data, isLoading } = useGetIGPostsQuery('all');
   return (
     <>
-      {data?.map(post => {
-        const {
-          id,
-          account,
-          location,
-          avatar,
-          photo,
-          likes,
-          description,
-          hashTags,
-          createTime
-        } = post;
-        return (
-          <IGPost
-            key={id}
-            account={account}
-            location={location}
-            avatar={avatar}
-            photo={photo}
-            likes={likes}
-            description={description}
-            hashTags={hashTags}
-            createTime={createTime}
-          />
-        );
-      })}
+      {isLoading ? (
+        <div className="flex aspect-square animate-pulse items-center justify-center bg-gray-100 pb-4 shadow-md lg:mb-8">
+          <Loading />
+        </div>
+      ) : (
+        data?.map(post => {
+          const {
+            id,
+            account,
+            location,
+            avatar,
+            photo,
+            likes,
+            description,
+            hashTags,
+            createTime
+          } = post;
+          return (
+            <IGPost
+              key={id}
+              account={account}
+              location={location}
+              avatar={avatar}
+              photo={photo}
+              likes={likes}
+              description={description}
+              hashTags={hashTags}
+              createTime={createTime}
+            />
+          );
+        })
+      )}
     </>
   );
 };
