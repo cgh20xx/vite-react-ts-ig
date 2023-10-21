46bbcae63a86472dda766e520acddec80eeb33d6
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-10-21 10:42:15 +0800

    feat: 新增 getIGPosts 的 endpoints 打動態牆的 API

--- src/services/homeServices.ts
+++ src/services/homeServices.ts
@@ -12,6 +12,7 @@ export const homeApi = createApi({
   reducerPath: 'homeApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3004/' }),
   endpoints: builder => ({
+    // 取得限時動態
     getIGStories: builder.query<IGStory[], string | 'all'>({
       query: id => {
         if (id === 'all') {
@@ -19,10 +20,19 @@ export const homeApi = createApi({
         }
         return `stories/${id}`;
       }
+    }),
+    // 取得動態牆
+    getIGPosts: builder.query({
+      query: id => {
+        if (id === 'all') {
+          return `posts/`;
+        }
+        return `posts/${id}`;
+      }
     })
   })
 });
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetIGStoriesQuery } = homeApi;
+export const { useGetIGStoriesQuery, useGetIGPostsQuery } = homeApi;
