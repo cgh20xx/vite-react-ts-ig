6fa6672c0d8ba6df8cbac0d7ba14bd3a9596832d
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-10-24 17:44:09 +0800

    feat: 新增 hooks 重新包裝 useDispatch 及 useSelector，目的是需要提供 TS 型別。

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,6 +3,7 @@ import { homeApi } from 'services/homeServices';
 import friendReducer from 'slices/friendSlice';
 
 export const store = configureStore({
+  // 補充：RTK 會將 reducer 傳遞給 combineReducers()，不再需要使用 combineReducers 包多個 reducer。
   reducer: {
     [homeApi.reducerPath]: homeApi.reducer,
     friendReducer
@@ -11,5 +12,9 @@ export const store = configureStore({
     return getDefaultMiddleware().concat(homeApi.middleware);
   }
 });
+
+// 因使用 typescript hooks.ts 需用到這兩個型別，故導出。
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
