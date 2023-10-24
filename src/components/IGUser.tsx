86a2ca69e9f52eba07a40f0c512b99209bdbfa7c
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-10-25 00:56:38 +0800

    refactor: 使用 memo() 避免 IGUser 切換 follow 狀態時 rerender。

--- src/components/IGUser.tsx
+++ src/components/IGUser.tsx
@@ -1,5 +1,6 @@
 import { useAppDispath } from 'hooks';
 import { follow, unFollow } from 'slices/friendSlice';
+import { memo } from 'react';
 
 type IGUserProps = {
   id?: string;
@@ -11,60 +12,62 @@ type IGUserProps = {
   isFollow?: boolean;
 };
 
-const IGUser: React.FC<IGUserProps> = ({
-  id,
-  account,
-  location,
-  avatar,
-  size = 'small',
-  showFollow = false,
-  isFollow = false
-}) => {
-  const dispatch = useAppDispath();
+const IGUser: React.FC<IGUserProps> = memo(
+  ({
+    id,
+    account,
+    location,
+    avatar,
+    size = 'small',
+    showFollow = false,
+    isFollow = false
+  }) => {
+    const dispatch = useAppDispath();
 
-  function followClickHandler() {
-    console.log('click follow id:', id);
-    if (id === undefined) return;
-    if (isFollow) {
-      dispatch(unFollow(id));
-    } else {
-      dispatch(follow(id));
+    function followClickHandler() {
+      console.log('click follow id:', id);
+      if (id === undefined) return;
+      if (isFollow) {
+        dispatch(unFollow(id));
+      } else {
+        dispatch(follow(id));
+      }
     }
-  }
-  return (
-    <div className="flex h-[70px] items-center px-4">
-      <div
-        className={`${
-          size === 'small' ? 'h-[40px] w-[40px]' : 'h-[60px] w-[60px]'
-        } rounded-full`}
-        style={{
-          backgroundImage: `url(${avatar})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center'
-        }}
-      ></div>
-      <div className="ml-4">
-        <p className="text-sm font-bold">{account}</p>
-        <p className="text-xs text-gray-400">{location}</p>
+    return (
+      <div className="flex h-[70px] items-center px-4">
+        <div
+          className={`${
+            size === 'small' ? 'h-[40px] w-[40px]' : 'h-[60px] w-[60px]'
+          } rounded-full`}
+          style={{
+            backgroundImage: `url(${avatar})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+          }}
+        ></div>
+        <div className="ml-4">
+          <p className="text-sm font-bold">{account}</p>
+          <p className="text-xs text-gray-400">{location}</p>
+        </div>
+        {showFollow &&
+          (isFollow ? (
+            <p
+              className="ml-auto cursor-pointer text-xs font-bold text-gray-700"
+              onClick={followClickHandler}
+            >
+              FOLLOWING
+            </p>
+          ) : (
+            <p
+              className="ml-auto cursor-pointer text-xs font-bold text-blue-400"
+              onClick={followClickHandler}
+            >
+              FOLLOW
+            </p>
+          ))}
       </div>
-      {showFollow &&
-        (isFollow ? (
-          <p
-            className="ml-auto cursor-pointer text-xs font-bold text-gray-700"
-            onClick={followClickHandler}
-          >
-            FOLLOWING
-          </p>
-        ) : (
-          <p
-            className="ml-auto cursor-pointer text-xs font-bold text-blue-400"
-            onClick={followClickHandler}
-          >
-            FOLLOW
-          </p>
-        ))}
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default IGUser;
